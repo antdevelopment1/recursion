8fcc1afc170e90d79280353a37ee13c4f7091441
April Copes <antdevelopment1@gmail.com>
2019-02-15 20:01:15 -0500

    Example of recursion

---
+++ firstRecur.js
@@ -0,0 +1,14 @@
+// Log out a count down based on the number passed recursivly
+// function countDown(num) {
+//     if (num <= 0) {
+//         console.log("I am done");
+//         return;
+//     } else {
+//         console.log(num)
+//         num--
+//         countDown(num)
+//     }
+// }
+
+
+// countDown(5)
