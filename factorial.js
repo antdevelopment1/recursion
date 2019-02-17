0a1da7722b1666b46e9edb3cac6ee969af0dd6d3
April Copes <antdevelopment1@gmail.com>
2019-02-16 22:58:52 -0500

    solve factorial recursivly

---
+++ factorial.js
@@ -0,0 +1,9 @@
+// Determine the factorial of a function iteratively
+function factorial(n) {
+    let total = 1;
+    for (let i = n; i > 1; i--) {
+        return n * (n - 1)
+    }
+}
+
+console.log(factorial(3))
