dd094d774de89975081a651ea14d3452d827a571
April Copes <antdevelopment1@gmail.com>
2019-02-17 20:56:03 -0500

    we collect the odds values revursivly and by using a recursive helper function

--- collectOddsRecur.js
+++ collectOddsRecur.js
@@ -29,3 +29,28 @@ function getOdds(arr) {
 }
 
 console.log(getOdds([1,2,3,4,5,6,7,8]));
+
+// Collect the odd numbers with pure recursion
+// Create a function called getOdds that accepts an arrat
+function getOdds(arr){
+    // Create an empty array
+    let newArr = [];
+    
+    // We check to see if the length of the array is equal to 0
+    if(arr.length === 0) {
+        // If it we return new array
+        return newArr;
+    }
+    // We check to see if the first value in the array is an odd number 
+    if(arr[0] % 2 !== 0){
+        // If it is we are going to push that number into the new array
+        newArr.push(arr[0]);
+    }
+    // We add to the new array the new value and delete that value from the old array and save it to a variable. We do not mutate the array by using concat  
+    newArr = newArr.concat(getOdds(arr.slice(1)));
+
+    // We return the new array
+    return newArr;
+}
+
+getOdds([1,2,3,4,5])
