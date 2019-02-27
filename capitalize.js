5f37a3df1e47743c47a2b776ef167637ec70861b
April Copes <antdevelopment1@gmail.com>
2019-02-26 20:29:00 -0500

    refactors recursive solution to use a method that returns a new array

--- capitalize.js
+++ capitalize.js
@@ -3,15 +3,17 @@ function capitalizeWords (array) {
     if (array.length === 1) {
         return [array[0].toUpperCase()];
     }
-    // We set to a variable the value of the last character of the array by using the slice method
+    // We set to a variable the value of everything but                                               the last character of the array by using the slice method
     let res = capitalizeWords(array.slice(0, -1));
-    // We push into a variable called results and use a slice method to take the last letter in our array
-    // being at the 0th index and uppercase each letter. We are only saving the last letter in our array
-    // which would be the 0th index and we are then able to uppercase that value
-    res.push(array.slice(array.length-1)[0].toUpperCase());
-    // We return the result with our pushed values
-    return res;
-    	 
+    let final = array.slice(array.length-1)[0].toUpperCase();
+    let newArray = res.concat(final);
+  
+    return newArray; 	 
 }
 
-console.log(capitalizeWords(["Hello how are you there"]))
+function helperFunction(array) {
+    return array.join("");
+}
+
+console.log(helperFunction(capitalizeWords("hello how are you there")))
+
